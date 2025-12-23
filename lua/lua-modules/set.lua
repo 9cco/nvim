0d5783590decc9675a26da2cd7a86282c9f130b3
9cco <nicolai.krohg@gmail.com>
2025-12-23 12:09:50 +0100

    feature: add custom statusline and buffer shortcuts

--- lua/lua-modules/set.lua
+++ lua/lua-modules/set.lua
@@ -30,3 +30,29 @@ vim.diagnostic.config({
     severity_sort = true,
 })
 
+local function GitBranch()
+    local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -d '\n'")
+    if string.len(branch) > 0 then
+        return branch
+    else
+        return ":"
+    end
+end
+
+local function statusline()
+    local setColor1 = "%#PmenuSel#"
+    local branch = GitBranch()
+    local setColor2 = "%#LineNr#"
+    local staticStatusString = "%n %f %m%r%h%w %= %{&fileencoding?&fileencoding:&encoding} %y %p%% %l:%c"
+
+    return string.format(
+        "%s %s %s %s",
+        setColor1,
+        branch,
+        setColor2,
+        staticStatusString
+    )
+end
+
+-- Make statusline show buffer number and percentage
+vim.o.statusline = statusline()
